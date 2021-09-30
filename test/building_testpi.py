995fa884ec7bc7dea704af5890d876414599aa32
yassinhc <yassin.hajj.chehade@gmail.com>
2021-09-30 10:48:03 +0200

    first test

--- test/building_testpi.py
+++ test/building_testpi.py
@@ -1,12 +1,16 @@
-#from Building import Building
+
+import sys
+sys.path.append('..')
+
 from src import Building
+#from Building import Building
 
 import unittest
 
 
 class Test_Building(unittest.TestCase):
     def test_Building_empty(self):
-        building = Building()
+        building = Building.Building([])
         nb = building.getLenArea()
         self.assertEqual(nb,0)
 
