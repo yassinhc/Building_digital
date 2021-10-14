af5e3cdee66cee1b99537bb73fcd8e4984ec8bbc
k0raty <91521656+k0raty@users.noreply.github.com>
2021-10-14 12:19:02 +0200

    Update patio.py

---
+++ src/patio.py
@@ -0,0 +1,8 @@
+from Area import Area
+
+class Patio(Area): #4 murs d'angles qqc
+    def getAngle(self): #retourne une liste des angles entre chaque mur contigus
+        Walls=[self.listElement[0]] #on crée une liste qui retrie nos murs selon leurs contiguité.
+        Walls.append( i for i self.listElement if Walls[-1].__coordinates[1]= i.__coordinates[0])]
+    def getSurface(self): 
+        
