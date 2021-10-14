d49cb01c2c8be3d78b5103c9ba692c7e216f43b9
k0raty <91521656+k0raty@users.noreply.github.com>
2021-10-14 12:05:36 +0200

    Update Room.py

--- src/Room.py
+++ src/Room.py
@@ -3,8 +3,8 @@ from Area import Area
 class Room(Area):
     #on suppose que les
     def getSurface(self): 
-        Longueurs=[element.lenght for element in Area.listElement if element.lenght not in Longueurs] #c'est un rectangle,on récupère les deux longueurs diff si ce n'est pas un carré.
+        Longueurs=[element.lenght for element in self.listElement if element.lenght not in Longueurs] #c'est un rectangle,on récupère les deux longueurs diff si ce n'est pas un carré.
         if len(Longueurs) == 1: #C'est un carré
-            return Area.listElement[0].lenght**2
+            return self.listElement[0].lenght**2
         else: return Longueurs[0]*Longueurs[1]
         
