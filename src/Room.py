7fc5c221a1a664a6e00872f9959abcfdb8e1b6f0
k0raty <91521656+k0raty@users.noreply.github.com>
2021-10-14 11:56:59 +0200

    Update Room.py
    Création de get surface pour des pièces rectangulaires.

--- src/Room.py
+++ src/Room.py
@@ -1,6 +1,10 @@
 from Area import Area
 
 class Room(Area):
-    
-    def getSurface(self):
+    #on suppose que les
+    def getSurface(self): 
+        Longueurs=[element.lenght for element in Area.listElement if element.lenght not in Longueurs] #c'est un rectangle,on récupère les deux longueurs diff si ce n'est pas un carré.
+        if len(Longueurs) == 1: #C'est un carré
+            return Area.listElement[0].lenght**2
+        else: return Longueurs[0]*Longueurs[1]
         
