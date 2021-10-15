3cbf39c54596084ceef9e6b6319a6dcd92fbb18d
k0raty <91521656+k0raty@users.noreply.github.com>
2021-10-15 14:05:25 +0200

    Update Wall.py

--- src/Wall.py
+++ src/Wall.py
@@ -7,8 +7,8 @@ from src.Element import Element
 
 class Wall(Element):
     def __init__(self):
-        P1,P2=getCoordinates(self)[0],getCoordinates(self)[1]
-        self.__vector=getVector(P1,P2) #vecteur associé au mur
+        P1,P2=self.getCoordinates()[0],self.getCoordinates()[1]
+        self.__vector=P1.getVector(P2) #vecteur associé au mur
      
     def getVector(self):
         return self.__vector 
