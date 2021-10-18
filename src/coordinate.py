0550923835a779a1808fe3a49be422e19687e32f
yassinhc <yassin.hajj.chehade@gmail.com>
2021-10-18 22:38:51 +0200

    add documentation to coordinate class

--- src/coordinate.py
+++ src/coordinate.py
@@ -3,39 +3,95 @@ from math import sqrt
 import numpy as np
 
 class Coordinate():
+    """
+    A class to represent a coordinate.
+    ...
+    
+    Attributes
+    ----------
+    x : int
+        the abscissa of the coordinate
+    y : int
+        the ordinate of the coordinate
+
+    Methods
+    -------
+    getx():
+        Get the abscissa of the coordinate.
+    gety():
+        Get the ordinate of the coordinate.
+    getVecor(coordinate):
+        Return the vector from current coordinate to the new one.
+    getLength(coordinate):
+        Return the length of two coordinate
+    isAfter(coordinate):
+        Return True if the coordinate is after our current coordinate.
+    """
     
     def __init__(self,x,y):
         self.__x=x
         self.__y=y
 
     def getx(self):
+        '''
+        Returns
+        -------
+        TYPE Integer
+            return the abscissa of a coordiante
+        '''
         return self.__x
 
     def gety(self):
+        '''
+        Returns
+        -------
+        TYPE Integer
+            return the ordinate of a coordiante
+        '''
         return self.__y
     
-    def getVector(self,coordinate): #from self to coordinate
+    def getVector(self,coordinate):
+        '''
+        Parameters
+        ----------
+        coordinate : Coordiante
+            The coordinate to compute the Vector with
+
+        Returns
+        -------
+        TYPE : NUMPY.ARRAY
+            return the vector from self to coordinate
+        '''
         X=coordinate.getx()-self.getx()
         Y=coordinate.gety()-self.gety()
         return(np.array([X,Y]))
     
     def getLength(self,coordinate):
-       vector= self.getVector(coordinate)
-       return(sqrt(vector[0]**2 + vector[1]**2)) #norm of the vector 
+        '''
+        Parameters
+        ----------
+        coordinate : Coordiante
+            The coordinate to compute the length with
+
+        Returns
+        -------
+        TYPE : Integer
+            return the length between two coordinates
+        '''
+        vector= self.getVector(coordinate)
+        return(sqrt(vector[0]**2 + vector[1]**2)) #norm of the vector 
    
     def isAfter(self,coordinate):
         '''
-
         Parameters
         ----------
         coordinate : Coordinate
-            The coordiante to comapre with
+            The coordiante to comapre if it is after our current coordinate or not
 
         Returns
         -------
         TYPE : Boolean
             return true if coordiante is after our current coordinate
-
         '''
         return coordinate.getx() >= self.getx() and coordinate.gety() >= self.gety()
     
--- test/coordianateTest.py
+++ test/coordianateTest.py
@@ -4,9 +4,9 @@ Created on Fri Oct  8 14:36:21 2021
 
 @author: user
 """
-import os
 
 
+import numpy as np
 
 import sys
 sys.path.append('..')
@@ -15,14 +15,13 @@ from src import coordinate
 
 import unittest
 
-print(os.getcwd())
 
 
 class Test_Building(unittest.TestCase):
+    
     def test_coordinates_length(self):
         coordinate1 = coordinate.Coordinate(0,1)
         coordinate2 = coordinate.Coordinate(0,0)
-        print(coordinate1.getx())
         nb = coordinate1.getLength(coordinate2)
         self.assertEqual(nb,1)
     
@@ -30,7 +29,17 @@ class Test_Building(unittest.TestCase):
         coordinate2 = coordinate.Coordinate(1,-2)
         self.assertTrue(coordinate2.getx()==1 and coordinate2.gety()==(-2))
                 
- 
+    def test_vector(self):
+        coordinate1 = coordinate.Coordinate(1,1)
+        coordinate2 = coordinate.Coordinate(3,0)
+        vect = coordinate1.getVector(coordinate2)
+        self.assertEqual(vect.all(),np.array([2,-1]).all())
+
+    def test_isAfter(self):
+        coordinate1 = coordinate.Coordinate(1,0)
+        coordinate2 = coordinate.Coordinate(3,0)
+        self.assertTrue(coordinate1.isAfter(coordinate2))
+        
 
 if __name__ == '__main__':
     unittest.main()
