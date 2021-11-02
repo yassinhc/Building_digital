6a0affe477d2d76d43787e71eae44c479669fd61
amalchaoui <amal.ch.mp@gmail.com>
2021-11-02 22:47:37 +0100

    fix: fixed a bug in convex_hull.py

--- building_map/library/convex_hull.py
+++ building_map/library/convex_hull.py
@@ -1,10 +1,13 @@
 import numpy as np
 from math import atan2
 
+def cross(o, a, b):
+    return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
 
 def lowest_coordinate(points):
     # find a point with lowest y-coordinate
-    # if there is 2, pick the one with lowest x-coordinate
+    # # # if there is 2, pick the one with lowest x-coordinate
     anchor_point = points[0]
     for i, point in enumerate(points):
         if point[1] < anchor_point[1]:
@@ -13,13 +16,10 @@ def lowest_coordinate(points):
             anchor_point = point
     return anchor_point
 
-
 def polar_angle(p0, p1):
     y_span=p0[1]-p1[1]
     x_span=p0[0]-p1[0]
     return atan2(y_span,x_span)
-
-
 
 def sort_angle(points):
     # sorting the points based on the angle they make with the x-axis
@@ -37,39 +37,55 @@ def sort_angle(points):
 
     return sorted_points
 
-
-def right_turn(a, b, c):
-    # returns if moving from a to c passing by b defines a right turn
-    return (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1]) <=0
     
-
 def convex_hull(points):
-    #1 : sorting the points with respect of their angle with the x-axis
-    anchor_point = lowest_coordinate(points)
-    sorted_points = sort_angle(points)
+    """Computes the convex hull of a set of 2D points.
+    """
+    # sorting the points based on the angle they make with the x-axis
+    points = sort_angle(points)
 
-    # initializing the convex hull with the anchor_point and the first point in the sorted list 
-    # of points with respect to angles 
-    cv_hull = [anchor_point, sorted_points[0]]  
+    # Base case: no points or a single point, possibly repeated multiple times.
+    if len(points) <= 1:
+        return points
 
-    for point in sorted_points[1:]:
-        # if right turn --> delete the previous point
-        while right_turn(cv_hull[-2], cv_hull[-1], point):
-            del cv_hull[-1]
-        cv_hull.append(point)
-
-    return cv_hull
+    # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+    # Returns a positive value, if OAB makes a counter-clockwise turn,
+    # negative for clockwise turn, and zero if the points are collinear.
 
 
+    # Build lower hull 
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
 
-import random 
-import matplotlib.pyplot as plt
+    # Build upper hull
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+
+    # Concatenation of the lower and upper hulls gives the convex hull.
+    # Last point of each list is omitted because it is repeated at the beginning of the other list. 
+    result = np.array(lower[:-1] + upper[:-1])
+    result = sort_angle(result)
+    result = np.append(result, [result[0]], axis = 0)
+    return result
 
 
-number_points = 50
-points = np.random.randint(1, 100, size = (number_points, 2))
-cv_hull = np.array(convex_hull(points))
+if __name__ == "__main__":
 
-plt.scatter(points[:, 0], points[:, 1])
-plt.plot(cv_hull[:, 0], cv_hull[:, 1], c = "r")
-plt.show()
+    import random 
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    number_points = 10
+    points = np.random.randint(1, 100, size = (number_points, 2))
+    cv_hull = convex_hull(points)
+    print(type(cv_hull))
+
+    plt.scatter(points[:, 0], points[:, 1])
+    plt.plot(cv_hull[:, 0], cv_hull[:, 1], c = "r")
+    plt.show()
