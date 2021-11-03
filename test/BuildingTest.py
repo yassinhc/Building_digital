313c4b17a2a736ae80b9acc171c6cd83cb545ff1
yassinhc <yassin.hajj.chehade@gmail.com>
2021-11-03 19:26:06 +0100

    feat: update Building

---
+++ test/BuildingTest.py
@@ -0,0 +1,21 @@
+
+import sys
+sys.path.append('..')
+
+from src import Building
+
+import unittest
+
+
+
+class Test_Building(unittest.TestCase):
+    
+    def test_Building_empty(self):
+        building = Building.Building([])
+        nb = building.getLenFloor()
+        self.assertEqual(nb,0)
+
+ 
+
+if __name__ == '__main__':
+    unittest.main()
