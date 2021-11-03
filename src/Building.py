a7c545720f8095ba37bc8b74ec537d55f335041c
amalchaoui <amal.ch.mp@gmail.com>
2021-11-03 16:28:48 +0100

    feat: update Building and add Floor classes

--- src/Building.py
+++ src/Building.py
@@ -1,19 +1,63 @@
-
-import src.Element as Element
-
 class Building():
-    def __init__(self,listElement):
-        """
-        """
+    """
+    A class to represent a Building.
+    ...
     
-        self.__listElement= listElement
+    Attributes
+    ----------
+    listFloor : list of Floor
+        The list of floors contained in the building
+    
+    Methods
+    -------
+    getListFloor():
+        Return the list of floors contained in the building
+    getLenFloor():
+        Return the number of floors in the building
+    addFloor(floor):
+        Add floor to the list if not exists
+    """
+    def __init__(self,listFloor):
+    
+        self.__listFloor= listFloor
+        
 
-    def getLenElement(self):
-        return len(self.__listElement)
+    def getLenFloor(self):
+        """
+        
+        Returns
+        -------
+        TYPE Integer
+            Return the number of floors in the building.
+
+        """
+        return len(self.__listFloor)
          
-    def getListElement(self):
-        return self.__listElement
+    def getListFloor(self):
+        """
+        
+        Returns
+        -------
+        TYPE List of Floor
+            Return the list of floor in the building.
+
+        """
+        return self.__listFloor
     
-    def addElement(self,element):
-        self.__listElement.append(element)
+    def addFloor(self,floor):
+        """
+    
+        Parameters
+        ----------
+        floor : Floor
+            The floor to add to the building.
+
+        Returns
+        -------
+        None. (add floor to the building if not exists)
+
+        """
+        if floor not in self.getListFloor():
+            self.__listFloor.append(floor)
+
 
