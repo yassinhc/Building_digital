9613fcf99483b4cfeb0c503cb48d7eebeddbc3fd
amalchaoui <amal.ch.mp@gmail.com>
2021-11-03 23:38:31 +0100

    feat: update SubArea with getPoints() method

--- src/SubArea.py
+++ src/SubArea.py
@@ -1,17 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 14 12:04:20 2021
 
-@author: user
-"""
-
-from Area import Area
+from src.Area import Area
+from src.coordinate import Coordinate
+import numpy as np
 
 class SubArea(Area):
         def __init__(self,listArea):
             self.__listArea=listArea
-            self.__res=self.
-            super().__init__()
+            self.__res=0
+            self.__listElements=[]
+            super().__init__([])
         
         def getListArea(self):
             return self.__listArea
@@ -19,10 +16,30 @@ class SubArea(Area):
         def addArea(self,area):
             if area not in self.__listArea:
                 self.__listArea.append(area)
+                self.computeSurface(area)
+        
+        def computeSurface(self,area):
+            self.__res+=area.getSurface()
         
         def getSurface(self):
             return self.__res
         
-        def calculateArea(self):
-            for area in self.__listArea:
-                
+        def getListElements(self):
+            return self.__listElements
+        
+        def addElement(self,element):
+            if element not in self.getListElements():
+                self.__listElements.append(element)
+
+        def getPoints(self):
+            points = []
+            for element in self.__listElements:
+                coord1, coord2 = element.getCoordinates()[0], element.getCoordinates()[1]
+                x1, y1, x2, y2 = coord1.getx(), coord1.gety(), coord2.getx(), coord2.gety()
+                if [x1, y1] not in points:
+                    points.append([x1, y1])
+                if [x2, y2] not in points:
+                    points.append([x2, y2])
+            return np.array(points)
+
+
