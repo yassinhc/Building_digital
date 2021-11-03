e78906558ba9437124a9a97190f5d4dbaee121af
amalchaoui <amal.ch.mp@gmail.com>
2021-11-03 14:20:36 +0100

    dosc: document k_means.py module

--- building_map/library/k_means.py
+++ building_map/library/k_means.py
@@ -1,13 +1,14 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-#X : the input should be a number of points  (the path of visitors in the building)
+# X : the input should be a number of points  (the path of visitors in the building)
 # TODO : We assume data is a numpy array containing points in 2D 
 
 
 def initializing_centroids(data, nb_centroids):
-    """Initializes nb_centroids-Centroids from the input data 
+    """Initializes centroids by randomly picking 'nb_centroids' of points for data 
 
     Parameters
     ----------
@@ -74,12 +75,61 @@ def closest_centroids(cent, X):
 
 
 def reinit_centroids(clusters, k, X):
+    """ Reinitialising centroids by computing the average of points with respect to each centroid 
+        and move the centroid to that average
+
+    Parameters
+    ----------
+    clusters : numpy array 2D
+        assigned cluster for each point in X
+    k : int
+        number of clusters
+    X : numpy array of 2D 
+        array of points
+
+    Returns
+    -------
+    numpy array of 2D
+        new centroids 
+    """
     new_centroids = np.zeros(shape = (k, 2))  
     for j in range(k):
-        current_cluster = np.array([X[i] for i in range(X.shape[0]) if clusters[0][i] == j])
-        cluster_mean = np.mean(current_cluster, axis = 0)
+        current_cluster = np.array([X[i] for i in range(X.shape[0]) if clusters[0][i] == j])   # points belonging to the j-th cluster
+        cluster_mean = np.mean(current_cluster, axis = 0)                                      # computing the mean point of the cluster's points
         new_centroids[j] = cluster_mean
     return new_centroids
+
+
+def plot_clusters(X, k, iter): 
+    """ Clusters points in X into k clusters using iter-nb of iterations and plots the clusters  
+
+    Parameters
+    ----------
+    X : numpy array of 2D
+        array of points 
+    k : int 
+        number of clusters
+    iter : int
+        number of iterations 
+    """
+    init_cent = initializing_centroids(X, k)             # initializing centroids
+    clos_cent = closest_centroids(init_cent, X)          # closest centroids ot each point
+
+    for i in range(iter):
+        clos_cent = closest_centroids(init_cent, X)      # updating closest centroids 
+        init_cent = reinit_centroids(clos_cent, k, X)    # updating centroids 
+
+    # ------ plotting ------
+    color = plt.cm.rainbow(np.linspace(0, 1, k)) 
+
+    pt_classes = dict()                                  # dictinary of points fitting into each cluster 
+
+    for j, c in zip(range(k), color):
+        pt_classes[j] = np.array([X[i] for i in range(X.shape[0]) if clos_cent[0][i]==j])
+        plt.scatter(pt_classes[j][:, 0], pt_classes[j][:, 1], color = c, marker = '.')
+
+    plt.scatter(init_cent[:, 0], init_cent[:, 1], color = 'black')  # plotting final centroids 
+
 
 
 if __name__ == "__main__":
@@ -87,41 +137,31 @@ if __name__ == "__main__":
     import numpy as np
     import matplotlib.pyplot as plt 
 
-    # generating a random numpy array
-    X= -2 * np.random.rand(150,2)
-    X1 = 1 + 2 * np.random.rand(50,2)
-    X2 = 3 + 2*np.random.rand(50,2)
-    X[50:100, :] = X1
-    X[100:150, :] = X2
-    #plt.scatter(X[:, 0], X[:, 1])
-    #plt.show()
-
-    k = 3
-
-    init_cent = initializing_centroids(X,k )
-    print(init_cent)
-
-    #clos_cent = closest_centroids(init_cent, X)
-    #print(clos_cent)
-
-    N = 4
-    for i in range(N):
-        clos_cent = closest_centroids(init_cent, X)
-        init_cent = reinit_centroids(clos_cent, k, X)
+    # ----- params -----
+    k = 3                   # number of clusters 
+    iter = 8                # number of iterattions
     
 
+    # ----- random points generation ----- 
+    nb_samples = 100 
+
+    low = [1, 1]
+    high = [10, 10]
+    X0 = np.random.default_rng().uniform(low, high, size = (nb_samples, 2))     # 1st list of points generated using uniform distribution 
+
+    mean1 = [3, 3]
+    cov1 = [[3, 0], [0, 3]]
+    X1 = np.random.default_rng().multivariate_normal(mean1, cov1, nb_samples)   # 2nd list of points using multivariate_normal distribution
+
+    mean2 = [7, 7]
+    cov2 = [[1, 0], [0, 1]]
+    X2 = np.random.default_rng().multivariate_normal(mean2, cov2, nb_samples)   # 3rd list of points using multivariate_normal distribution
+
+    X = np.append(X0, X1, axis = 0)           # concatenating the  list of points
+    X = np.append(X, X2, axis = 0)
 
 
-
-    pt_class1 = np.array([X[i] for i in range(X.shape[0]) if clos_cent[0][i]==0])
-    pt_class2 = np.array([X[i] for i in range(X.shape[0]) if clos_cent[0][i]==1])
-    pt_class3 = np.array([X[i] for i in range(X.shape[0]) if clos_cent[0][i]==2])
-
-    # plotting clusters 
-    plt.scatter(pt_class1[:, 0],pt_class1[:, 1], color = 'red')
-    plt.scatter(pt_class2[:, 0],pt_class2[:, 1], color = 'green')
-    plt.scatter(pt_class3[:, 0],pt_class3[:, 1], color = 'yellow')
-
-    plt.scatter(init_cent[:, 0], init_cent[:, 1], color = 'black')
+    # ----- plotting -----
+    plot_clusters(X, k, iter)                 # computing and plotting the clusters of points 
 
     plt.show()
